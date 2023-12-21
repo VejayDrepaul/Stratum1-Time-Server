834d526a1cc342b60387046f3a7502b30c5e3b69
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-12-21 17:45:08 -0500

    seven seg header files

---
+++ include/seven_segment.hpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#define D1 9
+#define D2 10
+#define D3 11
+#define D4 12
+#define A 8
+#define F 7
+#define B 6
+#define G 5
+#define C 4
+#define DP 3
+#define D 2
+#define E 13
+
+void zero(int digit);
+void one(int digit);
+void two(int digit);
+void three(int digit);
+void four(int digit);
+void five(int digit);
+void six(int digit);
+void seven(int digit);
+void eight(int digit);
+void nine(int digit);
+void reset();
+void calibrate();
